94af2b932539506519cd265e8cb0a5949f410ffe
habs <brenohabib2013@gmail.com>
2025-09-24 14:39:54 -0300

    gitignore     - updated .gitignore

---
+++ myproject/apps/upload/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+
+def upload_pdf(request):
+    if request.method == 'POST' and request.FILES.get('pdf_file'):
+        pdf_file = request.FILES['pdf_file']
+        messages.success(request, f'Arquivo "{pdf_file.name}" enviado com sucesso!')
+        return HttpResponseRedirect(reverse('upload_pdf'))
+    return render(request, 'upload/upload.html')
