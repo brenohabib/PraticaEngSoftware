6ef6084c610d1f1f86e39f1b91447353e7042aea
habs <brenohabib2013@gmail.com>
2025-09-24 14:41:36 -0300

    merge views.py

--- myproject/apps/upload/views.py
+++ myproject/apps/upload/views.py
@@ -5,11 +5,7 @@ from django.contrib import messages
 
 def upload_pdf(request):
     if request.method == 'POST' and request.FILES.get('pdf_file'):
-<<<<<<< HEAD
         pdf_file = request.FILES['pdf_file']
-=======
-        pdf_file = request.FILES['pdf_file']                
->>>>>>> upload_screen
         messages.success(request, f'Arquivo "{pdf_file.name}" enviado com sucesso!')
         return HttpResponseRedirect(reverse('upload_pdf'))
     return render(request, 'upload/upload.html')
